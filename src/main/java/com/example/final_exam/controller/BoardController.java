a579395812a3be8965d616e65fade606fec93f59
johnmok <johnmok99@naver.com>
2022-11-23 18:55:26 +0900

    test 4

--- src/main/java/com/example/final_exam/controller/BoardController.java
+++ src/main/java/com/example/final_exam/controller/BoardController.java
@@ -16,7 +16,6 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/board")
 @RequiredArgsConstructor
 public class BoardController {
-
     private final BoardService boardService;
     @GetMapping(value = "/write")
     public String boardWriteForm(Model model){
